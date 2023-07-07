1dc8e926010744fc32e7aee46c6f8f812332cb75
julia1234456 <57895833+julia1234456@users.noreply.github.com>
2023-07-07 19:32:39 +0200

    sctipts to prepare the dataset for detectron2/GTR

---
+++ prepare_frames_fishVal.py
@@ -0,0 +1,22 @@
+import cv2
+
+video = cv2.VideoCapture("fish_NOT_CROPPED.mp4")
+
+
+total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+num_frames = 1800
+
+
+frame_index = 0
+while frame_index < num_frames:
+    video.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
+    ret, frame = video.read()
+    if not ret:
+        break
+    if frame_index > 1439:
+        frame_name = "frame_"+ str(frame_index).zfill(6) + ".PNG"
+        cv2.imwrite(frame_name, frame)
+
+    frame_index += 1
+
+video.release()
